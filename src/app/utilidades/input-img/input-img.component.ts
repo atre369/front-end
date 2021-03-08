83dcab3092f8b986b8b63446ef2ec28248eabf2e
Atreyu <moralesatreyu@hotmail.com>
2021-03-08 01:21:18 -0600

    casi no funciona la cosa

--- src/app/utilidades/input-img/input-img.component.ts
+++ src/app/utilidades/input-img/input-img.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { toBase64 } from '../utilidades';
 
 @Component({
   selector: 'app-input-img',
@@ -9,7 +10,17 @@ export class InputImgComponent implements OnInit {
 
   constructor() { }
 
+  imagenBase64!: string;
+
   ngOnInit(): void {
+  }
+//podria causar bug ¡¡¡AQUI!!!
+  change(event: any){
+    if (event.target.files.length > 0){
+      const file: File = event.target.files[0];
+      toBase64(file).then((value: any) => this.imagenBase64 = value)
+      .catch((error: any) => console.log(error));
+    }
   }
 
 }
